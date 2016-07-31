b8ac9bc8ff7d6043b656a1d4ca471fee6dd51159
Kevin <kevin.wilhelm@outlook.at>
2016-07-31 16:35:03 +0200

    Ugly Login form :)

--- src/AppBundle/Controller/AdminLoginController.php
+++ src/AppBundle/Controller/AdminLoginController.php
@@ -1,26 +1,28 @@
 <?php
 namespace AppBundle\Controller;
 
-use AppBundle\Entity\Contentuser;
+
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\BrowserKit\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class AdminLoginController extends Controller
 {
 
     /**
-     * @param $name
+     *
      * @return \Symfony\Component\HttpFoundation\Response
      * @Route("/thelogingiant/login",name="adminlogin")
      */
     public function loginAction(Request $request)
     {
-        $user = new Contentuser();
-        //get the error
-        $authActionUtil = $this->get('security.authentication_utils');
-        
 
-        return $this->render('', array('name' => 1));
+        $authentification =$this->get('security.authentication_utils');
+        $error=$authentification->getLastAuthenticationError();
+        $lastU=$authentification->getLastUsername();
+
+        return $this->render("AdminLogin/Login.html.twig",array('last_username'=>$lastU,'error'=>$error));
+
+
     }
 }
