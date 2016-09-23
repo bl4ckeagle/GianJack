5a030770350edea9ea6af79455089abf6c11c66f
Kevin <kevin.wilhelm@outlook.at>
2016-09-23 11:52:19 +0200

    User implemented, mainfunction done, some bug fixes if arrays are empty. **** Hurray!!!!
    ****

--- app/framework/js/angular/productive/app.js
+++
@@ -1,7 +0,0 @@
-var phonecatApp = angular.module('phonecatApp', []);
-
-phonecatApp.controller('PhoneListCtrl', function ($scope,$http) {
-    $http.post('/angular/data').success(function(data)
-    {$scope.phones=data});
-    $scope.orderProp='age';
-});
