8c8adc514c27c9f132ea29de77b9bacfb1978ddc
bl4ckeagle <kevin.wilhelm@outlook.at>
2016-12-13 23:31:48 +0100

    bugs bugs bugs, user is now better implemented something like edit is now saving and assert for duplicated names

--- src/AppBundle/Form/DesignFormType.php
+++ src/AppBundle/Form/DesignFormType.php
@@ -4,7 +4,6 @@ namespace AppBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use Symfony\Component\Form\Extension\Core\Type\RadioType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -15,24 +14,38 @@ class DesignFormType extends AbstractType
     {
 
 
-            $Themes[]=$options["data"][0]['Themes'];
-            $Styles[]=$options["data"][0]['Style'];
+       foreach ($options["data"][0]['Style'] as $style) {
 
+            $explode = explode('.', $style);
+            $Styles[$explode[0]] = $style;
+        }
 
-        
+        foreach ($options["data"][0]['Themes'] as $theme) {
+
+            $explode = explode('.', $theme);
+            $Themes[$explode[0]] = $theme;
+        }
+
 
         $builder
-            ->add("Theme",ChoiceType::class,array("expanded"=>true,"multiple"=>false,'choices'=>$Themes,))
-            ->add("Style",ChoiceType::class,array("expanded"=>true,"multiple"=>false,'choices'=>$Styles,))
-             ->add('save',SubmitType::class,array(
-                          'attr' => array('class' => 'save')));
-        dump($options);
+            ->add(
+                "Theme", ChoiceType::class, array("expanded" => true,
+                "multiple" => false,
+                'choices' => $Themes,
+            ))
+            ->add(
+                "Style", ChoiceType::class, array("expanded" => true,
+                "multiple" => false,
+                'choices' => $Styles,
+
+            ))
+            ->add('save', SubmitType::class, array(
+                'attr' => array('class' => 'save')));
 
     }
 
     public function configureOptions(OptionsResolver $resolver)
     {
-
 
 
     }
